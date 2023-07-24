2c830d1be2baa1af9e7333a13c7f224f73553357
Eric Bailey <ericwbailey@users.noreply.github.com>
2023-07-24 14:42:42 +0000

    Update scope URL and QuerySelector

--- src/js/content.js
+++ src/js/content.js
@@ -2,7 +2,7 @@
 	// Adding a delay before chacking
 	setTimeout(function(){
 		// Check if the desktop interstitial is open
-		var interstitial = window.document.querySelectorAll("*[class^=\"desktop_app_interstitial\"]")[0]
+		var interstitial = window.document.querySelectorAll("#zoom-ui-frame")[0]
 		if(interstitial) {
 			chrome.runtime.sendMessage({closeThis: true});
 		}
